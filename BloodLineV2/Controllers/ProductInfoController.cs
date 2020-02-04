ec259267608d870fe36d1bc62dfb76ccfd4346be
Veera Sekaran Nadarajan <veera.nadarajan@gmail.com>
2020-02-04 23:46:26 +0800

    Creating product information sheets

---
+++ BloodLineV2/Controllers/ProductInfoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BloodLineV2.Views.ProductInfo
+{
+    public class ProductInfoController : Controller
+    {
+        // GET: ProductInfo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
