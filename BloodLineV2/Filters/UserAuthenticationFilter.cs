1f38a4eda5cb583c19444cbb3eec1457dde9da13
Veera Sekaran Nadarajan <veera.nadarajan@gmail.com>
2020-06-11 09:18:26 +0800

    Added AuthenticationFilter

---
+++ BloodLineV2/Filters/UserAuthenticationFilter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+
+namespace BloodLineV2.Filters
+{
+    public class UserAuthenticationFilter : ActionFilterAttribute, IAuthenticationFilter
+    {
+        void IAuthenticationFilter.OnAuthentication(AuthenticationContext filterContext)
+        {
+            //Check Session is Empty Then set as Result is HttpUnauthorizedResult 
+            if (string.IsNullOrEmpty(Convert.ToString(filterContext.HttpContext.Session["UserID"])))
+            {
+                filterContext.Result = new HttpUnauthorizedResult();
+            }
+        }
+
+        void IAuthenticationFilter.OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+            if (filterContext.Result == null || filterContext.Result is HttpUnauthorizedResult)
+            {
+                filterContext.Result = new ViewResult
+                {
+                    ViewName = "Error"
+                };
+            }
+        }
+    }
+}
